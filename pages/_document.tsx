666f59144901ddd225a44461f32c092d3b5be944
Daniel González Briseño <qbixmex@gmail.com>
2023-02-13 19:03:30 -0600

    nextui-document-added

---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import { CssBaseline } from '@nextui-org/react';
+
+class MyDocument extends Document {
+  static async getInitialProps(context: DocumentContext) {
+    const initialProps = await Document.getInitialProps(context);
+    return {
+      ...initialProps,
+      styles: React.Children.toArray([initialProps.styles])
+    };
+  }
+
+  render() {
+    return (
+      <Html lang="es">
+        <Head>
+          {CssBaseline.flush()}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,18 @@
-import { Button } from '@nextui-org/react';
 import { FC } from 'react';
+import { Button } from '@nextui-org/react';
+import Head from 'next/head';
 
 const HomePage: FC = () => {
   return (
-    <main>
-      <h1 className="title gray-dark">Pokedex App</h1>
-      <Button color="gradient">Button</Button>
-    </main>
+    <>
+      <Head>
+        <title>Pokedex App</title>
+      </Head>
+      <main>
+        <h1 className="title gray-dark">Pokedex App</h1>
+        <Button color="gradient">Button</Button>
+      </main>
+    </>
   );
 };
 

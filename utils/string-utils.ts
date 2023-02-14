c797664f217e6e66bb86efe4d84adf696c7dc1ba
Daniel González Briseño <qbixmex@gmail.com>
2023-02-14 08:39:14 -0600

    capitalize-documentation

--- utils/string-utils.ts
+++ utils/string-utils.ts
@@ -1,3 +1,7 @@
-export const capitalize = (name: string) => {
+/**
+ * Capitalize given text
+ * @returns Example: "dragon" becomes "Dragon"
+ */
+export const capitalize = (name: string): string => {
   return name.slice(0, 1).toUpperCase() + name.slice(1);
 };

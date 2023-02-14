bb0c63bf11249b5016949d0844a1979280abb264
Daniel González Briseño <qbixmex@gmail.com>
2023-02-13 18:47:43 -0600

    nextui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@nextui-org/react';
 import { FC } from 'react';
 
 const HomePage: FC = () => {
   return (
     <main>
-      <h1 className="title blue">Pokedex App</h1>
+      <h1 className="title gray-dark">Pokedex App</h1>
+      <Button color="gradient">Button</Button>
     </main>
   );
 };

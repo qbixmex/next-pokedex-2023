d22b47392a4b6541c5fed0b02eeaab8e59bb4168
Daniel González Briseño <qbixmex@gmail.com>
2023-02-13 22:05:57 -0600

    pokemon-api-server-side-generation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,24 @@
-import { FC } from 'react';
-import { GetStaticProps } from 'next';
+import { GetStaticProps, NextPage } from 'next';
 import { Layout } from '../components/layouts';
 import { pokeApi } from '../api';
-import { PokemonListResponse } from '../interfaces';
+import { PokemonListResponse, SmallPokemon } from '../interfaces';
+import Image from 'next/image';
+import { capitalize } from '../utils';
 
-const HomePage: FC = (props) => {
-  console.log(props);
+type Props = {
+  pokemons: SmallPokemon[];
+};
 
+const HomePage: NextPage<Props> = ({ pokemons }) => {
   return (
     <Layout title='Pokemons List'>
       <main>
-        <ul>
-          <li>Pokemon</li>
-        </ul>
+          { pokemons.map(({id, name, img}) => (
+            <div key={id}>
+              <p># {id} - { capitalize(name) }</p>
+              <Image src={img} alt={name} width={50} height={50} />
+            </div>
+          ))}
       </main>
     </Layout>
   );
@@ -21,9 +27,15 @@ const HomePage: FC = (props) => {
 const getStaticProps: GetStaticProps = async (context) => {
   const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151');
 
+  const pokemons: SmallPokemon[] = data.results.map((pokemon, i) => ({
+    ...pokemon,
+    id: i + 1,
+    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${i + 1}.svg`
+  }));
+
   return {
     props: {
-      pokemons: data.results
+      pokemons
     },
   };
 };
---
+++ utils/string-utils.ts
@@ -0,0 +1,3 @@
+export const capitalize = (name: string) => {
+  return name.slice(0, 1).toUpperCase() + name.slice(1);
+};
